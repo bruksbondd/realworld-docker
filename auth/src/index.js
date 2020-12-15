23de539f18d0f19dc58c699afc891f811cc0b730
bruksbondd <bruksbond@gmail.com>
2020-12-15 11:38:03 +0200

    Ready docker composer

---
+++ auth/src/index.js
@@ -0,0 +1,38 @@
+const axios = require("axios");
+const express = require("express");
+const { port, host, db, apiUrl } = require("./configuration");
+const { connectDb } = require("./helpers/db");
+
+const app = express();
+
+app.get("/test", (req, res) => {
+  res.send("Our authentication server is working correctly");
+});
+
+app.get("/testwithapidata", (req, res) => {
+  axios.get(apiUrl + '/testapidata').then(response => {
+    res.json({
+      testapidata: response.data.testwithapi
+    })
+  })
+})
+
+app.get("/api/currentUser", (req, res) => {
+  res.json({
+    id: "1234",
+    email: "foo@gmail.com"
+  })
+});
+
+const startServer = () => {
+  app.listen(port, () => {
+    console.log(`Started authentication service on port ${port}`);
+    console.log(`Our host is ${host}`);
+    console.log(`Database url ${db}`);
+  });
+};
+
+connectDb()
+  .on("error", console.log)
+  .on("disconnected", connectDb)
+  .once("open", startServer);
